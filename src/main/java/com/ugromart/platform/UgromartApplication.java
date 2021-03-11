4e552a2a444e51ac9aac3031f73a3a1e546c1ef9
kstephen3@gmail.com <kstephen3@gmail.com>
2021-03-11 11:44:42 +0300

    initial commit

---
+++ src/main/java/com/ugromart/platform/UgromartApplication.java
@@ -0,0 +1,13 @@
+package com.ugromart.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgromartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgromartApplication.class, args);
+	}
+
+}
